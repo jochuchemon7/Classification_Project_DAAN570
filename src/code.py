05d3e16121bccbd65b7166d00beb11d088de8b13
Josue Perez Gomez <37346269+jochuchemon7@users.noreply.github.com>
2023-09-29 19:10:02 -0500

    Update and rename code to code.py

---
+++ src/code.py
@@ -0,0 +1,12 @@
+from sklearn import preprocessing
+import matplotlib.pyploy as plt
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import os
+
+dir1 = os.getcwd()
+print(dir1)
+
+
+
